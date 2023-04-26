dba43efcbeea3ef4bff60b3222dbcf1a79210ff6
Szymon Rogowski <szymon0r0official@gmail.com>
2023-04-27 00:56:57 +0200

    Done

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# awesomeProject
+# REST API written in golang for needs of university classes. AIM was to build Dockerfiles for building and testing, then writing Jenkins pipelines in Groovy to automatize whole process.
+
--- main_test.go
+++ main_test.go
@@ -59,3 +59,78 @@ func TestAddHandler(t *testing.T) {
 			rr.Body.String(), expected)
 	}
 }
+
+func TestSubtractHandler(t *testing.T) {
+	reqBody := []byte(`[5, 3]`)
+
+	req, err := http.NewRequest("POST", "/subtract", bytes.NewBuffer(reqBody))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(subtractHandler)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := "2\n"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestMultiplyHandler(t *testing.T) {
+	reqBody := []byte(`[2, 3]`)
+
+	req, err := http.NewRequest("POST", "/multiply", bytes.NewBuffer(reqBody))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(multiplyHandler)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := "6\n"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestDivideHandler(t *testing.T) {
+	reqBody := []byte(`[6, 3]`)
+
+	req, err := http.NewRequest("POST", "/divide", bytes.NewBuffer(reqBody))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(divideHandler)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := "2\n"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
